6fb540373ba0a38b47a8c38803eafab66c33d4bf
Shaun Dsouza <dsouzashaun717@gmail.com>
2023-04-05 02:08:51 +0530

    completed backend integration with ml model

--- flask-backend/models.py
+++ flask-backend/models.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from utils import predict_home, predict_away, predict_match_result
 
 app = Flask(__name__)
 
@@ -12,46 +12,6 @@ def responseFormatter(status_code, status, data, message):
         'message': message
     }
 
-teams = {
-    'Blackpool': 5,
-    'Liverpool': 18,
-    'Manchester United': 20,
-    'Stoke City': 28,
-    'Fulham': 13,
-    'Blackburn Rovers': 4,
-    'Manchester City': 19,
-    'Sunderland': 29,
-    'Bolton Wanderers': 6,
-    'Arsenal': 1,
-    'Birmingham City': 3,
-    'Tottenham Hotspur': 31,
-    'West Bromwich Albion': 33,
-    'West Ham United': 34,
-    'Aston Villa': 2,
-    'Everton': 12,
-    'Newcastle United': 22,
-    'Wigan Athletic': 35,
-    'Wolverhampton Wanderers': 36,
-    'Chelsea': 10,
-    'Swansea City': 30,
-    'Queens Park Rangers': 24,
-    'Norwich City': 23,
-    'Reading': 25,
-    'Southampton': 27,
-    'Crystal Palace': 11,
-    'Cardiff City': 9,
-    'Hull City': 15,
-    'Burnley': 8,
-    'Leicester City': 17,
-    'Watford': 32,
-    'AFC Bournemouth': 0,
-    'Middlesbrough': 21,
-    'Brighton and Hove Albion': 7,
-    'Huddersfield Town': 14,
-    'Sheffield United': 26,
-    'Leeds United': 16
-}
-
 
 @app.route('/')
 def index():
@@ -61,9 +21,7 @@ def index():
 def predict_match():
     home_team = request.form['home_team']
     away_team = request.form['away_team']
-    home_team_code = teams[home_team]
-    away_team_code = teams[away_team]
-    result = {'home_team_code': home_team_code, 'away_team_code': away_team_code}
+    result = predict_match_result(home_team, away_team)
     return responseFormatter(200, 'success', result, 'Match prediction successful')
 
 
---
+++ flask-backend/utils.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+file_path_1 = absolute_path + '/artifacts/clean_data.csv'
+file_path_2 = absolute_path + '/artifacts/pl_wodel.pickle'
+
+
+pl_data = pd.read_csv(file_path_1)
+home_pred_cols = ["goal_home_ft","home_team_code","possession_avg_H","shots_avg_H","passes_avg_H"]
+away_pred_cols = ["goal_away_ft","away_team_code","possession_avg_A","shots_avg_A","passes_avg_A"]
+
+Xhome = pl_data[home_pred_cols]
+Xaway = pl_data[away_pred_cols]
+
+# Load the pickled objects into memory
+with open(file_path_2, 'rb') as f:
+    knnr_home, knnr_away, sc_home, sc_away, teams = pickle.load(f)
+
+
+def predict_home(home_team_code,away_team_code):
+    # filter pl_data to include only the rows for the specified team code
+    pl_data_team = pl_data[pl_data["away_team_code"] == away_team_code]
+
+    target_seasons = ['20/21','15/16','16/17','17/18','18/19','19/20']
+    target_data_df = Xhome[(Xhome["home_team_code"] == home_team_code) &
+     (pl_data_team["away_team_code"] == away_team_code) &
+     (pl_data_team['season'].isin(target_seasons))]
+
+    avg_df = pd.DataFrame(target_data_df.mean()).transpose()
+
+    target_data_scaled_home = sc_home.transform(avg_df)
+
+    # make predictions using the trained model
+    prediction_home = knnr_home.predict(target_data_scaled_home)
+
+    # # print the predicted values
+    return np.ceil(prediction_home).astype(int)[0]
+
+
+def predict_away(home_team_code,away_team_code):
+    target_seasons = ['20/21','15/16','16/17','17/18','18/19','19/20']
+    # filter pl_data to include only the rows for the specified team code
+    pl_data_team = pl_data[pl_data["home_team_code"] == home_team_code]
+
+    # find the latest year for the specified team
+    target_data_df = Xaway[(Xaway["away_team_code"] == away_team_code) &
+     (pl_data_team["home_team_code"] == home_team_code) &
+     (pl_data_team['season'].isin(target_seasons))]
+
+    avg_df = pd.DataFrame(target_data_df.mean()).transpose()
+
+    target_data_scaled_away = sc_away.transform(avg_df)
+
+    # make predictions using the trained model
+    prediction_away = knnr_away.predict(target_data_scaled_away)
+    return np.ceil(prediction_away).astype(int)[0]
+
+
+def predict_match_result(home_team_name,away_team_name):
+    home_team_code = teams[home_team_name]
+    away_team_code = teams[away_team_name]
+
+    home_team_result = predict_home(home_team_code,away_team_code)
+    away_team_result = predict_away(home_team_code,away_team_code)
+
+    print( {'home_team_result': home_team_result, 'away_team_result': away_team_result})
+    return {'home_team_result': int(home_team_result), 'away_team_result': int(away_team_result)}
+
