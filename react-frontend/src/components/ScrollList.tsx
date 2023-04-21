1d50ab741cd28ec46a37889412797d0beb57b9d9
Abhinav <102739327+AbhinavHaridas@users.noreply.github.com>
2023-04-21 14:31:38 +0530

    Update ScrollList.tsx

--- react-frontend/src/components/ScrollList.tsx
+++ react-frontend/src/components/ScrollList.tsx
@@ -12,27 +12,6 @@ const ScrollList: React.FC<ScrollListProps> = ({ setTeam }) => {
   const [dragging, setDragging] = useState(false);
   const [dragStartY, setDragStartY] = useState<number | null>(null);
 
-    const handleScroll = () => {
-    const list = listRef.current as HTMLElement;
-    const items = list.querySelectorAll("ion-item");
-    const itemHeight = items[0].offsetHeight;
-    const middleIndex = Math.ceil(list.scrollTop / itemHeight);
-    const prevIndex = middleIndex - 1;
-    const middleItem = items[middleIndex];
-    const prevItem = items[prevIndex];
-    const nextItem = items[middleIndex + 1];
-
-    if (prevItem && prevItem.classList.contains("middle-item")) {
-      prevItem.classList.remove("middle-item");
-    }
-
-    if (nextItem && nextItem.classList.contains("middle-item")) {
-      nextItem.classList.remove("middle-item");
-    }
-
-    middleItem && middleItem.classList.add("middle-item");
-  };
-
   useEffect(() => {
     function handleMouseMove(e: MouseEvent) {
       if (!dragging || dragStartY === null) return;
